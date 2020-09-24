e189f4828944a8df8e22fc75dbac5ce3d4bff42d
Mark Ellis <markellisdev@gmail.com>
2020-09-24 18:18:44 -0500

    added PORT var and tested

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ app.get('/', (req, res) => {
     res.send("You've reached the right place!");
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get('/api/courses', (req, res) => {
+    res.send([1, 3, 5]);
+});
 
+//Proper way to assign PORT var
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Listening on port ${port}...`));
